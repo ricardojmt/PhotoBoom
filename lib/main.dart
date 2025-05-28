25d34b08c55fb520ef8213cd86cb5545c44fe733
Sergio <est.sergio.acardenas@unimilitar.edu.co>
2025-05-27 21:07:49 -0500

    cambio s0.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; //importa firebase core
+import 'firebase_options.dart'; //importa las opciones gráficas por firebase CLI
 import 'package:photoboom/core/app_colores.dart';
 import 'package:photoboom/screens/pb_correo_contrasena.dart';
 import 'package:photoboom/screens/pb_feed.dart';
@@ -6,7 +8,14 @@ import 'package:photoboom/screens/pb_inicio_sesion.dart';
 import 'package:photoboom/screens/pb_olvide_contrasena.dart';
 import 'package:photoboom/screens/pb_registrarse.dart';
 
-void main() {
+void main() async {
+  //Asegura que los bindings de Flutter estén inicializados
+  WidgetsFlutterBinding.ensureInitialized;
+
+  //Inicializa Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
